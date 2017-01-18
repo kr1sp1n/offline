6499cc2b80a0cccc9edac5b6c823154450b2d424
Krispin Schulz <krispinone@googlemail.com>
2017-01-18 21:31:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,11 @@ const { port } = config;
 app.use(cacheManifest({
   manifestPath: '/application.manifest',
   cdn: ['http://yui.yahooapis.com/pure/0.5.0/pure-min.css'],
-  files: [{
-    file: __dirname + '/public/js/foo.js',
-    path: '/js/foo.js'
-  }, {
-    dir: __dirname + '/public/css',
-    prefix: '/css/'
-  }, {
-    dir: __dirname + '/views',
-    prefix: '/html/',
-    ignore: function(x) { return /\.bak$/.test(x); },
-    replace: function(x) { return x.replace(/\.jade$/, '.html'); }
-  }],
+  files: [],
   networks: ['*'],
   fallbacks: []
 }));
 
 app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on ${port}`);
 });
